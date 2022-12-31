c44dbc7dad0d2aa36e7a425e03ab5aef8f703e7a
Lala <lala@MacBook-Air-de-Laryssa.local>
2022-12-30 21:43:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let sec = 15
+const startCounter = () => {
+    setInterval(counter, 1000)
+}
+
+const counter = () => {
+    if(sec >= 0 ){
+        document.getElementById('timer').innerText = sec--     
+    }
+    
+    if(sec == 0 ){
+        setTimeout(()=>{
+             window.location.href = 'https://chat.whatsapp.com/DO6kAjZpQI2DakYpy7swb4'
+        },1500)
+    }
+}
+
+ startCounter()
+
+// const startCounter = () => {
+//     let timer = 15
+//     setInterval(()=> {
+//         if (--timer > 0)
+//     })
+// }
